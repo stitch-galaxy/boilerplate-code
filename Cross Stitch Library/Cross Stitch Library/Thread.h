c509b260183de6943922296ad98c81e8cea5fcf7
etarasov <tarasov.e.a@gmail.com>
2013-02-18 02:51:08 +0400

    Model

---
+++ Cross Stitch Library/Cross Stitch Library/Thread.h
@@ -0,0 +1,13 @@
+//
+//  Thread.h
+//  Cross Stitch Library
+//
+//  Created by 123 on 18.02.13.
+//  Copyright (c) 2013 Tarasov Evgeny. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Thread : NSObject
+
+@end
