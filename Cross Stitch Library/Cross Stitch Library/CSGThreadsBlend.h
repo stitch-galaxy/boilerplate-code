819ef0cb1da457f1089b51e574ab0fec974228e6
etarasov <tarasov.e.a@gmail.com>
2013-03-14 00:53:19 +0400

    Model

---
+++ Cross Stitch Library/Cross Stitch Library/CSGThreadsBlend.h
@@ -0,0 +1,13 @@
+//
+//  ThreadsBlend.h
+//  Cross Stitch Library
+//
+//  Created by 123 on 13.03.13.
+//  Copyright (c) 2013 Tarasov Evgeny. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ThreadsBlend : NSObject
+
+@end
