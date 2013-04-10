0e9b78dc943bfa59f989a19b77fc5fc8f89248d9
unknown <tarasev@W-6C626DDEC262.rencap.com>
2013-04-10 16:10:42 +0400

    Model

---
+++ Cross Stitch Library/Cross Stitch Library/CSGStraightStitch.h
@@ -0,0 +1,35 @@
+//
+//  CSGStraightStitch.h
+//  Cross Stitch Library
+//
+//  Created by 123 on 06.04.13.
+//  Copyright (c) 2013 Tarasov Evgeny. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "CSGDesignPoints.h"
+#import "CSGThreadsBlend.h"
+
+@protocol CSGStraightStitch <NSObject>
+
+@property (nonatomic, retain) CSGThreadsBlend *threadBlend;
+@property (nonatomic, retain) CSGDesignPoints *curve;
+
+@end
+
+@interface CSGStraightStitch : NSObject<CSGStraightStitch>
+
+-(id) initWithThreadsBlend: (CSGThreadsBlend*) aThreadBlend Curve:(CSGDesignPoints*) aCurve;
+
+@end
+
+@interface CSGStraightStitch (Serialization)
+
+- (size_t) serializedLength;
+- (void) serializeWithBinaryEncoder: (CSGBinaryEncoder *) anEncoder ThreadsPalette: (CSGThreadsPalette*) palette;
+- (id) initWithBinaryDecoder: (CSGBinaryDecoder*) anDecoder ThreadsPalette: (CSGThreadsPalette*) palette;
+
+@end
+
+
---
+++ Cross Stitch Library/Cross Stitch Library/CSGStraightStitch.m
@@ -0,0 +1,89 @@
+//
+//  CSGStraightStitch.m
+//  Cross Stitch Library
+//
+//  Created by 123 on 06.04.13.
+//  Copyright (c) 2013 Tarasov Evgeny. All rights reserved.
+//
+
+#import "CSGStraightStitch.h"
+
+@implementation CSGStraightStitch
+
+@synthesize threadBlend;
+
+@synthesize curve;
+
+-(id) initWithThreadsBlend: (CSGThreadsBlend*) aThreadBlend Curve:(CSGDesignPoints*) aCurve
+{
+    if (self = [super init])
+    {
+        threadBlend = aThreadBlend;
+        curve = aCurve;
+    }
+    return self;
+}
+
+
+- (BOOL) isEqual: (id) object
+{
+    if (object == self)
+    {
+        return YES;
+    }
+    if (!object || ![object isKindOfClass:[self class]])
+    {
+        return NO;
+    }
+    
+    return [self isEqualToCSGStraightStitch: object];
+}
+
+- (BOOL) isEqualToCSGStraightStitch: (CSGStraightStitch*) aStraightStitch
+{
+    if (self == aStraightStitch)
+    {
+        return YES;
+    }
+    if (![threadBlend isEqual:aStraightStitch.threadBlend] || ![curve isEqual:aStraightStitch.curve])
+    {
+        return NO;
+    }
+    
+    
+    return YES;
+}
+
+- (NSUInteger)hash
+{
+    NSUInteger hash = 17;
+    hash = hash*31 + threadBlend.hash;
+    hash = hash*31 + curve.hash;
+    
+    return hash;
+}
+
+@end
+
+@implementation CSGStraightStitch (Serialization)
+
+- (size_t) serializedLength
+{
+    return threadBlend.serializedLength + curve.serializedLength;
+}
+
+- (void) serializeWithBinaryEncoder: (CSGBinaryEncoder *) anEncoder ThreadsPalette: (CSGThreadsPalette*) palette
+{
+    [threadBlend serializeWithBinaryEncoder:anEncoder ThreadsPalette:palette];
+    [curve serializeWithBinaryEncoder:anEncoder];
+}
+
+- (id) initWithBinaryDecoder: (CSGBinaryDecoder*) anDecoder ThreadsPalette: (CSGThreadsPalette*) palette
+{
+    CSGThreadsBlend *aThreadsBlend = [[CSGThreadsBlend alloc] initWithBinaryDecoder:anDecoder ThreadsPalette:palette];
+    CSGDesignPoints *aCurve = [[CSGDesignPoints alloc] initWithBinaryDecoder:anDecoder];
+    
+    return [self initWithThreadsBlend:aThreadsBlend Curve:aCurve];
+}
+
+@end
