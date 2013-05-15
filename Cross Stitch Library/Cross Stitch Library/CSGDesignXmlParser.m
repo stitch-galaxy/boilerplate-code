490da7637b23116e6e3e9c30aea6fe66595971a9
etarasov <tarasov.e.a@gmail.com>
2013-05-16 02:44:29 +0400

    Model

---
+++ Cross Stitch Library/Cross Stitch Library/CSGDesignXmlParser.m
@@ -0,0 +1,63 @@
+//
+//  CSGDesignXmlParser.m
+//  Cross Stitch Library
+//
+//  Created by 123 on 15.05.13.
+//  Copyright (c) 2013 Tarasov Evgeny. All rights reserved.
+//
+
+#import "CSGDesignXmlParser.h"
+
+@interface CSGDesignXmlParser()
+
+@property (nonatomic, retain) CSGXmlParser *parser;
+
+@end
+
+@implementation CSGDesignXmlParser
+
+@synthesize parser;
+
+- (void) parse
+{
+    [parser parse];
+}
+
+- (id) initWithUrl: (NSURL*) anUrl delegate: (id<CSGXmlParserDelegate>) delegate
+{
+    if (self = [super init])
+    {
+        parser = [[CSGXmlParser alloc] initWithUrl:anUrl delegate:self];
+    }
+    return self;
+}
+
+- (void) elementFound:(NSString*) elementName
+       attributeNames:(NSArray*) attributeNames
+      attributeValues:(NSArray*) attributeValues
+{
+    NSLog(@"Element found: %@", elementName);
+    for (int i = 0; i < attributeNames.count; ++i)
+    {
+        NSLog(@"Attribute found: %@ value: %@", [attributeNames objectAtIndex:i], [attributeValues objectAtIndex:i]);
+    }
+}
+
+- (void) endElement:(NSString*) elementName
+               text:(NSString*)text
+{
+    NSLog(@"Element end: %@", elementName);
+    NSLog(@"Element text: %@", text);
+}
+
+- (void) error:(NSError*) error
+{
+    NSLog(@"Error happened: %@", error);
+}
+
+- (void) endDocument
+{
+    NSLog(@"End document");
+}
+
+@end
