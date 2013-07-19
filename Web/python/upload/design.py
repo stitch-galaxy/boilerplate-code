5d57653d8e4b1afe53df788343f870946a2b9e58
tarasov.e.a@gmail.com <tarasov.e.a@gmail.com>
2013-07-19 18:54:32 +0400

    Web

---
+++ Web/python/upload/design.py
@@ -0,0 +1,26 @@
+﻿class DesignLocalization:
+
+	def __init__(self):
+		self.designFilePath = None
+		self.descrpitionFilePath = None
+		self.jsonFilePath = None
+
+class Design:
+
+	def __init__(self):
+		self.dict = dict()
+
+	def getLocalization(self, localizationString):
+		if not(self.dict.has_key(localizationString)):
+			self.dict[localizationString] = DesignLocalization()
+		return self.dict[localizationString]
+
+class Designs:
+
+	def __init__(self):
+		self.dict = dict()
+
+	def getDesign(self, uuid):
+		if not(self.dict.has_key(uuid)):
+			self.dict[uuid] = Design()
+		return self.dict[uuid]
---
+++ Web/python/upload/structues.py
@@ -0,0 +1,16 @@
+﻿class DesignLocalization:
+
+	def __init__(self):
+		self.designFilePath = None
+		self.descrpitionFilePath = None
+		self.jsonFilePath = None
+
+class DesignToUpload:
+
+	def __init__(self):
+		self.dict = dict()
+
+	def getLocalization(self, localizationString):
+		if not(self.dict.has_key(localizationString)):
+			self.dict[localizationString] = DesignLocalization()
+        return self.dict[localizationString]
