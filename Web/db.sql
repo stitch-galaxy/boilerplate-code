f9e3e76102031f21e6d549b7377810073975439d
tarasov.e.a@gmail.com <tarasov.e.a@gmail.com>
2013-07-30 19:12:59 +0400

    web

--- Web/db.sql
+++ Web/db.sql
@@ -2,122 +2,193 @@ drop database StitchGalaxy;
 
 create database StitchGalaxy;
 
-USE StitchGalaxy;
+use StitchGalaxy;
 
 create table T_DESIGNS
 (
-DesignId VARBINARY(16),
+DesignUuid varbinary(16),
 ReleaseDate datetime default now() not null,
 Sales bigint default 0,
 TotalRating bigint default 0,
 TotalRates bigint default 0,
 Blocked bit null,
-primary key (DesignId)
+primary key (DesignUuid)
 );
 
 create table T_DESIGN_PARAMETERS
 (
 ParametersId bigint auto_increment,
-DesignId VARBINARY(16),
-LanguageId varchar(10) not null,
+DesignUuid varbinary(16),
+Language varchar(10) not null,
 Name nvarchar(255) not null,
 Description nvarchar(255) null,
 Width int null,
 Heigth int null,
 Colors int null,
-HasSmallImage bit not null,
-HasLargeImage bit not null,
+HasThumbnail bit not null,
+HasImage bit not null,
 HasWebDescription bit not null,
-HasDesignFile bit not null,
+HasDesign bit not null,
 primary key (ParametersId)
 );
 
 alter table T_DESIGN_PARAMETERS
-	add foreign key (DesignId)
-		references T_DESIGNS (DesignId);
+	add foreign key (DesignUuid)
+		references T_DESIGNS (DesignUuid);
+
+create table T_DESIGN_TAGS
+(
+	DesignUuid varbinary(16),
+	TagId int
+);
+
+alter table T_DESIGN_TAGS
+	add foreign key (DesignUuid)
+		references T_DESIGNS (DesignUuid);
 
 use stitchgalaxy;
 
-#select d.DesignId, d.ReleaseDate, p.Width, p.Heigth, p.Colors, p.Name, p.Description, p.ImageSmall, p.ImageLargeName, p.DescriptionUrl, p.DownloadUrl, dc.Sales, dc.AvgRating
-#from T_DESIGNS d 
-#INNER JOIN T_DESIGN_LOCALIZATION dl on dl.DesignId = d.DesignId
-#INNER JOIN T_PARAMETERS p on p.ParametersId = dl.ParametersId
-#LEFT OUTER JOIN T_DESIGN_COUNTERS dc on d.DesignId = dc.DesignId
-#WHERE dl.Language = 'en';
-
 DELIMITER $$
-CREATE PROCEDURE createOrUpdateDesignInformation
+create procedure createOrUpdateDesignInformation
 (
-IN guid char(16),
-IN releaseDate datetime,
-IN sales bigint,
-IN totalRating bigint,
-IN totalRates bigint
+in uuid char(16),
+in releaseDate datetime,
+in sales bigint,
+in totalRating bigint,
+in totalRates bigint,
+in blocked bit
 )
-BEGIN
+begin
 
-	DECLARE currReleaseDate datetime;
-	DECLARE curSales bigint;
-	DECLARE curTotalRating bigint;
-	DECLARE curTotalRates bigint;
+	declare designUuid varbinary(16);
+
+	declare curReleaseDate datetime;
+	declare curSales bigint;
+	declare curTotalRating bigint;
+	declare curTotalRates bigint;
+	declare curBlocked bit;
+
+	declare recordsFound int default 0;
+
+	set designUuid = hex(uuid);
+	
+	select count(*) into recordsFound from T_DESIGNS where DesignUuid = designUuid;
+	
+	if recordsFound = 1
+	then
+			
+		select ReleaseDate, Sales, TotalRating, TotalRates, Blocked into curReleasedate, curSales, cutTotalRating, curTotalRates, curBlocked from T_DESIGNS where DesignUuid = designUuid;
+
+		if releaseDate is null
+		then
+			set releaseDate = curReleaseDate;
+		end if;
+
+		if sales is null
+		then
+			set sales = curSales;
+		end if;
+
+		if totalRating is null
+		then
+			set totalRating = curTotalRating;
+		end if;
+
+		if totalRates is null
+		then
+			set totalRates = curTotalRates;
+		end if;
+
+		if blocked is null
+		then
+			set blocked = curBlocked;
+		end if;
 		
-	select ReleaseDate, Sales, TotalRating, TotalRates into currReleaseDate,  curSales, curTotalRating, curTotalRates from T_DESIGNS where DesignId = hex(guid);
-
-	if releaseDate is null
-	then
-		set releaseDate = currReleaseDate;
+		update T_DESIGNS 
+		set 
+			ReleaseDate = releaseDate,
+			Sales = sales,
+			TotalRating = totalRating,
+			TotalRates = totalRates,
+			Blocked = totalBlocked
+		where
+			DesignUuid = designUuid;
+	else
+		insert into T_DESIGNS(DesignUuid, ReleaseDate, Sales, TotalRating, TotalRates, Blocked)
+		values (designUuid, releaseDate, sales, totalRating, totalRates, blocked);
 	end if;
 
-	if sales is null
-	then
-		set sales = currSales;
-	end if;
-
-	if totalRating is null
-	then
-		set totalRating = curTotalRating;
-	end if;
-
-	if totalRates is null
-	then
-		set totalRates = curtTotalRates;
-	end if;
-	
-END$$
+end$$
 
 DELIMITER $$
-CREATE PROCEDURE updateDesignLocalization
+create procedure createOrUpdateDesignLocalization
 (
-IN guid char(16),
-IN languageId varchar(10),
-IN name varchar(255),
-IN description varchar(255),
+in uuid char(16),
+in language varchar(10),
+in name varchar(255),
+in description varchar(255),
 
-IN width int,
-IN height int,
-IN colors int,
+in width int,
+in height int,
+in colors int,
 
-IN hasDesignFile bit,
-IN hasDescriptionFile bit,
-IN hasSmallImage bit,
-IN hasLargeImage bit
+in hasThumbnail bit,
+in hasImage bit,
+in hasWebDescription bit,
+in hasDesign bit
 )
-BEGIN
+begin
 
-	DECLARE recordfound INT default 0;
+	declare designUuid varbinary(16);
+	declare parametersId int default null;
 
-	select count(*) into recordfound from T_DESIGNS where DesignId = hex(guid);
+	set designUuid = hex(uuid);
+
+	select ParametersId into parametersId from T_DESIGN_PARAMETERS where DesignUuid = designUuid and Language = language;
 	
-	if recordfound = 0
+	if parametersId is null
 	then
-		INSERT into T_DESIGNS(DesignId) values(hex(GUID));
+		insert into T_DESIGN_PARAMETERS(DesignUuid, Language, Name, Description, Width, Heigth, Colors, HasThumbnail, HasImage, HasWebDescription, HasDesign) 
+		values(designUuid, language, name, description, width, heigth, colors, hasThumbnail, hasImage, hasWebDescription, hasDesign);
+	else
+		update T_DESIGN_PARAMETERS set DesignUuid = designUuid, Language = language, Name = name, Description = description, Width = width, Heigth = heigth, Colors = colors, HasThumbnail = hasThumbnail, HasImage = hasImage, HasWebDescription = hasWebDescription, HasDesign = hasDesign 
+		where ParametersId = parametersId;
 	end if;
 
-	select count(*) into recordfound from T_DESIGN_LOCALIZATION where Language = Language and Region = region and DesignId = hex(guid);
+end$$
 
-	if recordfound = 0
+DELIMITER $$
+create procedure searchDesigns
+(
+in searchString nvarchar(255),
+in sortOrder int
+)
+begin
+
+#TOP_SALE = 1
+#MOST_RECENT = 2
+#TOP_RATE = 3
+#MOST_RELEVANT = 4
+#RANDOM = 5
+#PRICE_ASC = 6
+#PRICE_DESC = 7
+#COMPLEXITY_ASC = 8
+#COMPLEXITY_DESC = 9
+
+	declare designUuid varbinary(16);
+	declare parametersId int default null;
+
+	set designUuid = hex(uuid);
+
+	select ParametersId into parametersId from T_DESIGN_PARAMETERS where DesignUuid = designUuid and Language = language;
+	
+	if parametersId is null
 	then
-		INSERT into T_DESIGNS(DesignId) values(hex(GUID));
+		insert into T_DESIGN_PARAMETERS(DesignUuid, Language, Name, Description, Width, Heigth, Colors, HasThumbnail, HasImage, HasWebDescription, HasDesign) 
+		values(designUuid, language, name, description, width, heigth, colors, hasThumbnail, hasImage, hasWebDescription, hasDesign);
+	else
+		update T_DESIGN_PARAMETERS set DesignUuid = designUuid, Language = language, Name = name, Description = description, Width = width, Heigth = heigth, Colors = colors, HasThumbnail = hasThumbnail, HasImage = hasImage, HasWebDescription = hasWebDescription, HasDesign = hasDesign 
+		where ParametersId = parametersId;
 	end if;
 
-END$$
+end$$
