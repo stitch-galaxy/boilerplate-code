8c739ba611b06c8fa6dcd0a03e2d307355f2435a
tarasov.e.a@gmail.com <tarasov.e.a@gmail.com>
2013-09-17 11:52:09 +0400

    Web

--- Web/python/server/upload/upload.py
+++ Web/python/server/upload/upload.py
@@ -29,10 +29,45 @@ def copyFile(file, fileName, designGuid):
 
 def ConvertJsonToBson(designGuid, jsonDict):
 	bsonDict = copy.deepcopy(jsonDict)
+
+	#enrich with guid
 	bson["guid"] = designGuid
 
-	if jsonDict.has_key("releaseDate"):
-		bsonDict["releaseDate"] = datetime.strptime(jsonDict["releaseDate"], "%m-%d-%Y")
+    #convert to datetime to persist properly
+	if bsonDict.has_key("releaseDate"):
+		bsonDict["releaseDate"] = datetime.strptime(bsonDict["releaseDate"], "%m-%d-%Y")
+
+    #blocked property
+	if bsonDict.has_key("blocked") and bsonDict["blocked"]:
+		bsonDict["blocked"] = True
+	else:
+		bsonDict["blocked"] = False
+
+	if bsonDict.has_key("languages"):
+		languages = bsonDict["languages"]
+
+		for language in languages:
+			#thumbnail property
+			if language.has_key("hasThumbnail") and language["hasThumbnail"]:
+				language["hasThumbnail"] = True
+			else:
+				language["hasThumbnail"] = False
+            #image property
+			if language.has_key("hasImage") and language["hasImage"]:
+				language["hasImage"] = True
+			else:
+				language["hasImage"] = False
+            #design property
+			if language.has_key("hasDesign") and language["hasDesign"]:
+				language["hasDesign"] = True
+			else:
+				language["hasDesign"] = False
+            #html page property
+			if language.has_key("hasHtmlPage") and language["hasHtmlPage"]:
+				language["hasHtmlPage"] = True
+			else:
+				language["hasHtmlPage"] = False
+
 	return bsonDict
 
 class Upload:
