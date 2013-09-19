e57f2b2271937dff298d55fb71cde5da7abeb0ad
tarasov.e.a@gmail.com <tarasov.e.a@gmail.com>
2013-09-19 17:41:28 +0400

    Web

---
+++ Web/sg_web/client/upload/common.py
@@ -0,0 +1,12 @@
+﻿import requests
+
+def postRequest(files, requestParameters):
+	retries = 0
+	while retries != 3:
+		try:
+			r = requests.post(uploadUrl, files = files, data = requestParameters, timeout = 1000)
+			print "INFO: Success"
+			break
+		except:
+			print "ERROR: POST request failed"
+			retries = retries + 1
---
+++ Web/sg_web/client/upload/upload_config.py
@@ -0,0 +1,1 @@
+﻿serverUrl = "http://localhost:80"
