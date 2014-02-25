ca4ae9e0169a020e8061eefb3b157fdad844b3b4
tarasov.e.a@gmail.com <tarasov.e.a@gmail.com>
2014-02-25 16:21:19 +0400

    security

---
+++ Java/sg-manager-web/src/main/java/com/stitchgalaxy/sg_manager_web/LoginController.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.stitchgalaxy.sg_manager_web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+ 
+@Controller
+public class LoginController {
+ 
+	@RequestMapping(value="/login", method = RequestMethod.GET)
+	public String login(ModelMap model) {
+ 
+		return "login";
+ 
+	}
+ 
+	@RequestMapping(value="/loginfailed", method = RequestMethod.GET)
+	public String loginerror(ModelMap model) {
+ 
+		model.addAttribute("error", "true");
+		return "login";
+ 
+	}
+ 
+	@RequestMapping(value="/logout", method = RequestMethod.GET)
+	public String logout(ModelMap model) {
+ 
+		return "login";
+ 
+	}
+ 
+}
