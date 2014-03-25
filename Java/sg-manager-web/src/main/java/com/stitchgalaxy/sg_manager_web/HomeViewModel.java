c27cde13cdabe5f7468eada72527fe24cc550853
tarasov.e.a@gmail.com <tarasov.e.a@gmail.com>
2014-03-25 12:27:24 +0400

    web

--- Java/sg-manager-web/src/main/java/com/stitchgalaxy/sg_manager_web/HomeViewModel.java
+++ Java/sg-manager-web/src/main/java/com/stitchgalaxy/sg_manager_web/HomeViewModel.java
@@ -22,35 +22,3 @@ public class HomeViewModel {
     Product selected;
     List<Product> products = new ArrayList<Product>();
 }
-
-
-
- 
- 
- 
-public class ContributorViewModel {
-    private Contributor selected;
-    private List<String> titles = new ArrayList<String>(new ContributorData().getTitles());
-    private List<Contributor> contributors = new ArrayList<Contributor>(new ContributorData().getContributors());
- 
-    @Init
-    public void init() {    // Initialize
-        selected = contributors.get(0); // Selected First One
-    }
- 
-    public List<String> getContributorTitles() {
-        return titles;
-    }
- 
-    public List<Contributor> getContributorList() {
-        return contributors;
-    }
- 
-    public void setSelectedContributor(Contributor selected) {
-        this.selected = selected;
-    }
- 
-    public Contributor getSelectedContributor() {
-        return selected;
-    }
-}
