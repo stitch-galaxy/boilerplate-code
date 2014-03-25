17fd325c610db1e0e1b729426d4a4254bddedc44
edupard <tarasov.e.a@gmail.com>
2014-03-26 01:04:45 +0400

    web

--- Java/sg-manager-web/src/main/java/com/stitchgalaxy/sg_manager_web/HomeViewModel.java
+++ Java/sg-manager-web/src/main/java/com/stitchgalaxy/sg_manager_web/HomeViewModel.java
@@ -6,19 +6,63 @@
 
 package com.stitchgalaxy.sg_manager_web;
 
+import com.stitchgalaxy.dto.CommandGetProducts;
+import com.stitchgalaxy.dto.ProductInfo;
+import com.stitchgalaxy.service.DomainDataService;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.zkoss.bind.annotation.Init;
-import org.zkoss.util.CollectionsX;
+import org.zkoss.zk.ui.select.annotation.VariableResolver;
+import org.zkoss.zk.ui.select.annotation.WireVariable;
+import org.zkoss.zkplus.spring.SpringUtil;
 
 
 /**
  *
  * @author Administrator
  */
+@VariableResolver(org.zkoss.zkplus.spring.DelegatingVariableResolver.class)
 public class HomeViewModel {
 
-    Product selected;
-    List<Product> products = new ArrayList<Product>();
+    @WireVariable
+    DomainDataService dataservice;
+            
+    private Product selectedProduct;
+    private List<Product> products = new ArrayList<Product>();
+
+    /**
+     * @return the selectedProduct
+     */
+    public Product getSelectedProduct() {
+        return selectedProduct;
+    }
+
+    /**
+     * @param selectedProduct the selectedProduct to set
+     */
+    public void setSelectedProduct(Product selectedProduct) {
+        this.selectedProduct = selectedProduct;
+    }
+
+    /**
+     * @return the products
+     */
+    public List<Product> getProducts() {
+        return products;
+    }
+    
+    @Init
+    public void init(){
+        List<ProductInfo> products = dataservice.getAllProducts(new CommandGetProducts());
+        for (ProductInfo p : products)
+        {
+            Product product = new Product();
+            product.setId(p.getId());
+            product.setName(p.getName());
+            product.setPrice(p.getPriceUsd());
+            product.setDate(p.getDate().toString());
+            this.products.add(product);
+        }
+    }
 }
--- Java/sg-manager-web/src/main/java/com/stitchgalaxy/sg_manager_web/Product.java
+++ Java/sg-manager-web/src/main/java/com/stitchgalaxy/sg_manager_web/Product.java
@@ -6,10 +6,71 @@
 
 package com.stitchgalaxy.sg_manager_web;
 
+import java.math.BigDecimal;
+
 /**
  *
  * @author Administrator
  */
 public class Product {
-    
+    private Long id;
+    private String name;
+    private String date;
+    private BigDecimal price;
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the date
+     */
+    public String getDate() {
+        return date;
+    }
+
+    /**
+     * @param date the date to set
+     */
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    /**
+     * @return the price
+     */
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
 }
