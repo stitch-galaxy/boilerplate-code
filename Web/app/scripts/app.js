be1b480b8ad9f67de704b4e6f08342015f49663d
tarasov.e.a@gmail.com <tarasov.e.a@gmail.com>
2014-04-15 14:08:29 +0400

    Angular web

---
+++ Web/app/scripts/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+angular
+  .module('webApp', [
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/signin', {
+        templateUrl: 'views/signin.html',
+        controller: 'SigninCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
