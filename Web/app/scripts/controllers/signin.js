7f3c89c4296d35f6864ab3d47561d2f5d78ca119
tarasov.e.a@gmail.com <tarasov.e.a@gmail.com>
2014-04-15 18:57:17 +0400

    We

--- Web/app/scripts/controllers/signin.js
+++ Web/app/scripts/controllers/signin.js
@@ -1,5 +1,8 @@
 'use strict';
 
 angular.module('webApp')
-.controller('SigninCtrl', function ($scope) {
+.controller('SigninCtrl', function ($scope, $location) {
+  $scope.signin = function (user) {
+    $location.path('/');
+  };
 });
