79940fd565092e462e7ea946dcbd2625f6fa9381
tarasov.e.a@gmail.com <tarasov.e.a@gmail.com>
2014-04-18 19:09:41 +0400

    web

--- Web/test/spec/controllers/main.js
+++ Web/test/spec/controllers/main.js
@@ -1,9 +1,9 @@
 'use strict';
 
-describe('Controller: MainCtrl', function () {
+describe('Controller: HomeCtrl', function () {
 
   // load the controller's module
-  beforeEach(module('webApp'));
+  beforeEach(module('sgManager'));
 
   var MainCtrl,
     scope;
@@ -11,12 +11,12 @@ describe('Controller: MainCtrl', function () {
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
+    MainCtrl = $controller('HomeCtrl', {
       $scope: scope
     });
   }));
 
   it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+    expect(scope.awesomeThings).toBe(undefined);
   });
 });
