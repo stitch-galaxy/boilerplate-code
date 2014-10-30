4d7cf1e9eae090a6325c46248af6acfe10429446
tarasov.e.a@gmail.com <tarasov.e.a@gmail.com>
2014-10-30 17:05:28 +0300

    Single project

--- sg/sg-rest-api/src/main/java/com/sg/domain/spring/configuration/JpaContext.java
+++ sg/sg-rest-api/src/main/java/com/sg/domain/spring/configuration/JpaContext.java
@@ -8,14 +8,17 @@ package com.sg.domain.spring.configuration;
 import com.sg.domain.entities.jpa.CanvasesRepository;
 import com.sg.domain.entities.jpa.ProductRepository;
 import com.sg.domain.entities.jpa.ThreadsRepository;
+import java.util.Properties;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+//import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate4.HibernateExceptionTranslator;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -33,50 +36,56 @@ import org.springframework.transaction.support.TransactionTemplate;
 //GAE do not support @Transactional annotation because where is a class on a call stack which is not in a white list
 //@EnableTransactionManagement
 public class JpaContext {
-    
-    @Value( "${jdbc.url}" ) private String jdbcUrl;
-    @Value( "${jdbc.driverClassName}" ) private String driverClassName;
-    @Value( "${jdbc.username}" ) private String username;
-    @Value( "${jdbc.password}" ) private String password;
-    @Value( "${hibernate.dialect}" ) private String hibernateDialect;
-    @Value( "${hibernate.hbm2ddl.auto:}" ) private String hibernateHbm2ddlAuto;
-    @Value( "${hibernate.show_sql:false}" ) private Boolean hibernateShowSql;
-    @Value( "${hibernate.format_sql:false}" ) private Boolean hibernateFormatSql;      
-    
+
+    @Value("${jdbc.url}")
+    private String jdbcUrl;
+    @Value("${jdbc.driverClassName}")
+    private String driverClassName;
+    @Value("${jdbc.username}")
+    private String username;
+    @Value("${jdbc.password}")
+    private String password;
+    @Value("${hibernate.dialect}")
+    private String hibernateDialect;
+    @Value("${hibernate.hbm2ddl.auto:}")
+    private String hibernateHbm2ddlAuto;
+    @Value("${hibernate.show_sql:false}")
+    private Boolean hibernateShowSql;
+    @Value("${hibernate.format_sql:false}")
+    private Boolean hibernateFormatSql;
+
     @Bean
     public EntityManagerFactory entityManagerFactory() {
 
         HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        vendorAdapter.setGenerateDdl(true);
 
         LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
         factory.setJpaVendorAdapter(vendorAdapter);
         factory.setPackagesToScan("com.sg.domain.entities.jpa");
         factory.setDataSource(dataSource());
-        //no caching
-        factory.getJpaPropertyMap().put("cache.provider_class", "org.hibernate.cache.NoCacheProvider");
-        //show and format sql during logging
-        factory.getJpaPropertyMap().put("hibernate.show_sql", hibernateShowSql);
-        factory.getJpaPropertyMap().put("hibernate.format_sql", hibernateFormatSql);
-        //dialect
-        factory.getJpaPropertyMap().put("hibernate.dialect", hibernateDialect);
-        //db schema
-        if (hibernateHbm2ddlAuto != null && !hibernateHbm2ddlAuto.isEmpty())
-            factory.getJpaPropertyMap().put("hibernate.hbm2ddl.auto", hibernateHbm2ddlAuto);
-        factory.afterPropertiesSet();
 
-        return factory.getObject();
+        Properties jpaProperties = new Properties();
+        jpaProperties.put(org.hibernate.cfg.Environment.SHOW_SQL, hibernateShowSql);
+        jpaProperties.put(org.hibernate.cfg.Environment.FORMAT_SQL, hibernateFormatSql);
+        jpaProperties.put(org.hibernate.cfg.Environment.DIALECT, hibernateDialect);
+        if (hibernateHbm2ddlAuto != null && !hibernateHbm2ddlAuto.isEmpty()) {
+            jpaProperties.put(org.hibernate.cfg.Environment.HBM2DDL_AUTO, hibernateHbm2ddlAuto);
+        }
+
+        factory.setJpaProperties(jpaProperties);
+        
+        factory.afterPropertiesSet();
+        EntityManagerFactory result = factory.getObject();
+        return result;
     }
     
-    @Bean
-    public DataSource dataSource() {
-        org.apache.commons.dbcp.BasicDataSource ds = new org.apache.commons.dbcp.BasicDataSource();
-        ds.setUrl(jdbcUrl);
+    @Bean DataSource dataSource()
+    {
+        DriverManagerDataSource ds = new DriverManagerDataSource();
         ds.setDriverClassName(driverClassName);
+        ds.setUrl(jdbcUrl);
         ds.setUsername(username);
         ds.setPassword(password);
-        ds.setInitialSize(4);
-        ds.setDefaultAutoCommit(false);
         return ds;
     }
 
@@ -86,17 +95,16 @@ public class JpaContext {
         txManager.setEntityManagerFactory(entityManagerFactory());
         return txManager;
     }
-    
+
     @Bean
-    public TransactionTemplate transactionTemplate()
-    {
+    public TransactionTemplate transactionTemplate() {
         TransactionTemplate template = new TransactionTemplate(transactionManager());
         template.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
         return template;
     }
-    
-    @Bean 
-    public HibernateExceptionTranslator hibernateExceptionTranslator(){ 
-      return new HibernateExceptionTranslator(); 
+
+    @Bean
+    public HibernateExceptionTranslator hibernateExceptionTranslator() {
+        return new HibernateExceptionTranslator();
     }
 }
