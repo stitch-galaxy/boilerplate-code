08985c3e1ea78f9a583ea6835cf29144c0f8a001
tarasov.e.a@gmail.com <tarasov.e.a@gmail.com>
2015-09-03 16:10:06 +0300

    Security configuration: 1. Spring security in place 2. /ouath/token endpoint requires https 3. /** stateless without csrf protection permit all 4. other endpoint denyAll

--- rest/api/src/main/java/com/sg/rest/api/Application.java
+++ rest/api/src/main/java/com/sg/rest/api/Application.java
@@ -13,6 +13,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@SpringBootApplication(exclude = {
+//    org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class
+//})
 public class Application {
 
     public static void main(String[] args) {
---
+++ rest/api/src/main/java/com/sg/rest/api/WebSecurityConfig.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.rest.api;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+/**
+ *
+ * @author Admin
+ */
+@Configuration
+@EnableWebMvcSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static void configureStatelessSecurityWithoutCsrfProtection(HttpSecurity http) throws Exception {
+        //Stateless
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        //No csrf
+        http.csrf().disable();
+    }
+
+    @Configuration
+    @Order(1)
+    public static class GetTokenSecurityContextConfiguration extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http = http.antMatcher("/oauth/token");
+            configureStatelessSecurityWithoutCsrfProtection(http);
+
+            http.authorizeRequests().antMatchers("/**").permitAll()
+                    .and()
+                    .requiresChannel().anyRequest().requiresSecure();
+        }
+    }
+
+    @Configuration
+    @Order(2)
+    public static class TokenBasedSecurityContextConfiguration extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http = http.antMatcher("/**");
+            configureStatelessSecurityWithoutCsrfProtection(http);
+            http.authorizeRequests().antMatchers("/**").permitAll();
+        }
+    }
+
+    @Configuration
+    @Order(3)
+    public static class DenyAccessSecurityContextConfiguration extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .authorizeRequests()
+                    .anyRequest().denyAll();
+        }
+    }
+
+}
